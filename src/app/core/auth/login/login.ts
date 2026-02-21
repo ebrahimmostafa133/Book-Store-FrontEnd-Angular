05e6a362762ea4d8c9fab28830d724b8237d7b10
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-02-21 11:54:54 +0200

    updated components

--- src/app/core/auth/login/login.ts
+++ src/app/core/auth/login/login.ts
@@ -1,11 +1,36 @@
-import {Component} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {Component, inject, signal} from '@angular/core'
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
+import {AuthService} from '../../services/auth.service'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
 })
 export class Login {
+  private fb = inject(FormBuilder)
+  private authService = inject(AuthService)
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  })
+
+  constructor(private _fb: FormBuilder) {}
+
+  errorMessage = signal<string | null>(null)
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: () => console.log('Logged in!'),
+        error: (err) => {
+          this.errorMessage.set(err.error?.message
+            || 'Invalid email or password. Please try again.')
+        },
+      })
+    }
+  }
 }
