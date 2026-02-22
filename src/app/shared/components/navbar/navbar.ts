95f3ad290c8c923f6768532161a475c4373b5600
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-02-22 18:10:17 +0200

    generate a navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,15 +1,48 @@
-import type {OnInit} from '@angular/core'
-import {Component} from '@angular/core'
-import {initFlowbite} from 'flowbite'
+import {Component, inject} from '@angular/core'
+import {Router, RouterLink, RouterLinkActive} from '@angular/router'
+import {AuthService} from '../../../core/services/auth.service'
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar implements OnInit {
-  ngOnInit() {
-    initFlowbite()
+export class Navbar {
+  authService = inject(AuthService)
+  private router = inject(Router)
+
+  isMenuOpen = false
+  isDropdownOpen = false
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen
+  }
+
+  closeMenus() {
+    this.isMenuOpen = false
+    this.isDropdownOpen = false
+  }
+
+  isLoggedIn() {
+    return this.authService.isLoggedIn()
+  }
+
+  isAdmin() {
+    return this.authService.userRole === 'admin'
+  }
+
+  isUser() {
+    return this.authService.userRole === 'user'
+  }
+
+  logout() {
+    this.authService.logout()
+    this.closeMenus()
   }
 }
