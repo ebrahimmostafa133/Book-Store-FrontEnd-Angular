bea54f018c612ba409ab129d51cce01a4cc354cb
Ahmed Ehab <ahmedehab62003@gmail.com>
2026-02-22 16:03:13 +0200

    Make the home page using dummy data

--- src/app/core/interfaces/book.interface.ts
+++ src/app/core/interfaces/book.interface.ts
@@ -1,3 +1,14 @@
 export interface Book {
-
+  _id?: string
+  name: string
+  author: any // Can be string (ObjectId) or populated Author interface
+  category: any // Can be string (ObjectId) or populated Category interface
+  price: number
+  description?: string
+  bookCover: string
+  stock?: number
+  averageRating?: number
+  numReviews?: number
+  createdAt?: string
+  updatedAt?: string
 }
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,24 @@
-import {Component} from '@angular/core'
+import type {OnInit} from '@angular/core'
+import type {Book} from '../../core/interfaces/book.interface'
+import {CurrencyPipe} from '@angular/common'
+import {Component, inject} from '@angular/core'
+import {RouterLink} from '@angular/router'
+import {BooksService} from '../../core/services/books.service'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+  private readonly booksService = inject(BooksService)
+  books: Book[] = []
 
+  ngOnInit(): void {
+    // Get all books, sort them by rating (highest first), and keep only the top 8 for the "Trending" section
+    this.books = this.booksService.getBooks()
+      .sort((a, b) => (b.averageRating || 0) - (a.averageRating || 0))
+      .slice(0, 8)
+  }
 }
