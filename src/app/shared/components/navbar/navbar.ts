6770f0ba627f6f9661f389653de8c0f08b40529c
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-02-22 16:07:00 +0200

    generate the navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,15 +1,26 @@
-import type {OnInit} from '@angular/core'
-import {Component} from '@angular/core'
-import {initFlowbite} from 'flowbite'
+import {Component, computed, inject} from '@angular/core'
+import {Router, RouterLink, RouterLinkActive} from '@angular/router'
+import {AuthService} from '../../../core/services/auth.service'
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar implements OnInit {
-  ngOnInit() {
-    initFlowbite()
+export class Navbar {
+  authService = inject(AuthService)
+  router = inject(Router)
+
+  isLoggedIn = computed(() => this.authService.currentUser() !== null)
+  isAdmin = computed(() => this.authService.currentUser()?.role === 'admin')
+  isUser = computed(() => this.authService.currentUser()?.role === 'user')
+
+  isAuthPage() {
+    return this.router.url === '/login' || this.router.url === '/register'
+  }
+
+  logout() {
+    this.authService.logout()
   }
 }
