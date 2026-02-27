a97da645f58fae3f87daa65df718de215c24a910
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-02-27 14:40:10 +0200

    reverted platform additional check

--- src/app/core/guards/is-logged.guard.ts
+++ src/app/core/guards/is-logged.guard.ts
@@ -1,17 +1,11 @@
 import type {CanActivateFn} from '@angular/router'
-import {isPlatformBrowser} from '@angular/common'
-import {inject, PLATFORM_ID} from '@angular/core'
+import {inject} from '@angular/core'
 import {Router} from '@angular/router'
 import {AuthService} from '../services/auth.service'
 
 export const isLoggedGuard: CanActivateFn = () => {
   const router = inject(Router)
   const authService = inject(AuthService)
-  const platformId = inject(PLATFORM_ID)
-
-  if (!isPlatformBrowser(platformId)) {
-    return true
-  }
 
   if (!authService.decodedToken) { return true }
   if (authService.userRole === 'admin') { return router.parseUrl('/admin/dashboard') }
