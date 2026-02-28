5f67550b43024f5f7e5a4e7b700506f48bbbf647
ebrahimmostafa133 <ibrahim.mostafa9939@gmail.com>
2026-02-28 22:47:15 +0200

    deploying

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,22 @@ import {RenderMode} from '@angular/ssr'
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'authors/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
